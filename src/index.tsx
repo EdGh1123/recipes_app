0e7d8564a7a1312a2994f731b4816082fcb78db6
EdGh1123 <eduardodelahozguerrero@gmail.com>
2022-06-29 23:42:25 -0500

    Ajustes de Imagenes
    Ajustes de texto

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import './fuentes/Monster-Clubhouse.ttf';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
